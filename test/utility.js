314dc48cc4249b2aa4d2e1a26401b1ebde19730c
Richard Loa <richard.loa@cbc.ca>
2016-10-02 07:41:52 -0400

    adding test case for formatText method

--- test/utility.js
+++ test/utility.js
@@ -1,7 +1,15 @@
-describe("The utility methods", function() {
-  it("is odd", function() {
-    expect(window.util.isOdd("Richard")).toBeNull();
-    expect(window.util.isOdd(1)).toBe(true);
-    expect(window.util.isOdd(8)).toBe(false);
-  });
+describe("The utility method", function() {
+    it("isOdd", function() {
+        expect(window.util.isOdd("Richard")).toBeNull();
+        expect(window.util.isOdd(1)).toBe(true);
+        expect(window.util.isOdd(8)).toBe(false);
+    });
+
+    it("formatText", function(){
+        expect(window.util.formatText("Dragons' Den")).toBe("dragons-den");
+        expect(window.util.formatText("Murdoch Mysteries")).toBe("murdoch-mysteries");
+        expect(window.util.formatText("Mr. D")).toBe("mr-d");
+        expect(window.util.formatText("Kim's Convenience")).toBe("kims-convenience");
+        expect(window.util.formatText("Michael:Tuesdays & Thursdays")).toBe("michael-tuesdays-thursdays");
+    });
 });
