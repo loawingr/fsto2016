59be88e69e974901673da1d799a3a2322c16ee42
Richard Loa <richard.loa@cbc.ca>
2016-10-02 06:52:06 -0400

    changing file glob pattern matching to match nested directories

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './test/*.js',
-      './src/*.js'
+      './test/**/*.js',
+      './src/**/*.js'
     ],
 
 
