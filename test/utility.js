90a83aa2b17f4d1532687d72c4758959805f6811
Richard Loa <richard.loa@cbc.ca>
2016-10-02 06:57:07 -0400

    adding test case for non numbers

--- test/utility.js
+++ test/utility.js
@@ -1,5 +1,6 @@
 describe("The utility methods", function() {
   it("is odd", function() {
+    expect(window.util.isOdd("Richard")).toBeNull();
     expect(window.util.isOdd(1)).toBe(true);
     expect(window.util.isOdd(8)).toBe(false);
   });
