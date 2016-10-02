d99557df1de9b5774980dfc7f71a66749f832974
Richard Loa <richard.loa@cbc.ca>
2016-10-02 07:59:19 -0400

    passed formatText test

--- src/js/utility.js
+++ src/js/utility.js
@@ -6,7 +6,17 @@
                 return null;
 	    }
             return (n%2 !== 0);
-        }
+        },
+        formatText : function(str){
+	    if (typeof(str) !== "string"){
+	        return null;
+            }
+            str = str.toLowerCase().trim();
+            str = str.replace(/:/g, " ");
+            str = str.replace(/[^a-z0-9-\s]+/g, "");
+            str = str.replace(/\s+/g, "-");
+            return str;
+	}
     };
     if (!window.util){
         window.util = util;
