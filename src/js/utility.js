e4e64b4602e63ec59f420690b11ab18db5d33d57
Richard Loa <richard.loa@cbc.ca>
2016-10-05 18:57:36 -0400

    whitespace consistency

--- src/js/utility.js
+++ src/js/utility.js
@@ -4,19 +4,19 @@
         isOdd : function(n){
             if (typeof(n) !== "number"){
                 return null;
-	    }
+            }
             return (n%2 !== 0);
         },
         formatText : function(str){
-	    if (typeof(str) !== "string"){
-	        return null;
+            if (typeof(str) !== "string"){
+                return null;
             }
             str = str.toLowerCase().trim();
             str = str.replace(/:/g, " ");
             str = str.replace(/[^a-z0-9-\s]+/g, "");
             str = str.replace(/\s+/g, "-");
             return str;
-	}
+        }
     };
     if (!window.util){
         window.util = util;
